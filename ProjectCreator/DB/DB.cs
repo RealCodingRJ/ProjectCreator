c9e3109ade9cd64602a1417914828908884901c6
Priates of Zimsey <realcoder2023@gmail.com>
2025-09-17 16:39:34 -0400

    Add project files.

---
+++ ProjectCreator/DB/DB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace ProjectCreator.DB
+{
+    internal class DB
+    {
+
+        public static string? URI()
+        {
+            return "mongodb://localhost:27017";
+        }
+
+        public static async Task GetDB(string data)
+        {
+
+            string? URL = DB.URI();
+            var client = new MongoClient(URL);
+
+            var DB1 = client.GetDatabase("PROJECT");
+
+            var coll = DB1.GetCollection<BsonDocument>("Project_Name");
+            await coll.InsertOneAsync(new BsonDocument("Name", data));
+
+        }
+
+    }
+}
---
+++ ProjectCreator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+namespace ProjectCreator
+{
+
+    class Program
+    {
+        static async Task Main(string[] _)
+        {
+
+            Console.WriteLine("Enter Type:  ");
+            Console.WriteLine("React | Next | Spring");
+
+            var Project = Console.ReadLine();
+            if (Project == null) return;
+
+
+            if (Project.Equals("React"))
+            {
+                Thread.Sleep(1000);
+
+                Process.Start("cmd.exe", "npx nano-react-app myapp");
+                await DB.DB.GetDB("npx nano-react-app myapp");
+            }
+
+            if (Project.Equals("Next"))
+            {
+
+                Thread.Sleep(1000);
+                Process.Start("cmd.exe","npx npx create-next-app myapp");
+                await DB.DB.GetDB("npx npx create-next-app myapp");
+            }
+
+            if (Project.Equals("Spring"))
+            {
+
+                Thread.Sleep(1000);
+                Process.Start("cmd.exe", "spring init --dependencies=web,data-jpa myproject");
+                await DB.DB.GetDB("spring init --dependencies=web,data-jpa myproject");
+            }
+
+        }
+
+    }
+    
+}
